62654297fbb35b772d8aa68eeec9c3cd1c37de46
tf-yoshiyama-junya <yoshiyama@techfun.jp>
2023-09-14 09:22:30 +0900

    supported SQLAlchemy 2.x
    Dialect sqlite:pysqlite will not make use of SQL compilation caching as it does not set the 'supports_statement_cache' attribute to ``True``.

--- sqlalchemy_pysqlite3/__init__.py
+++ sqlalchemy_pysqlite3/__init__.py
@@ -2,6 +2,9 @@ from sqlalchemy.dialects import registry
 from sqlalchemy.dialects.sqlite.pysqlite import SQLiteDialect_pysqlite
 
 class SQLiteDialect_pysqlite3(SQLiteDialect_pysqlite):
+
+  supports_statement_cache = True
+  
   @classmethod
   def import_dbapi(cls):
     from pysqlite3 import dbapi2 as result
